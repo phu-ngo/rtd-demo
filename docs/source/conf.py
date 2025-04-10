2401ce60537ef2185f6a54553610a47900fa9fa4
Phu Ngo <quangphu1109@gmail.com>
2025-04-10 23:42:34 +0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'SAIGONESE MDM ORG'
+copyright = '2025, SAIGONESE'
+author = 'Phu'
 
 release = '0.1'
 version = '0.1.0'
