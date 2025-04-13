8e340b9335e3160e182d3b0983bb67a00089a124
Phu Ngo <quangphu1109@gmail.com>
2025-04-13 14:36:40 +0700

    Modify Language

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -47,7 +47,7 @@ language = 'en'  # Default language
 
 html_context = {
     'languages': [
-        ('en', '/'),
-        ('vi', '/vi/%s'),
+        ("English", "/%s/", 'en'),
+        ("TiengViet", "/vi/%s/", 'vi'),
     ],
 }
